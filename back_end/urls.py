74ba092034904e695bb716340a850e6ec12be731
EnLiao <pikachu200594@gmail.com>
2025-10-27 19:45:04 +0800

    fix: rename auth urls to user urls and edit readme

--- back_end/urls.py
+++ back_end/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('user.urls')), 
+    path('user/', include('user.urls')), 
     path('course/', include('courses.urls')),
 ]
 
