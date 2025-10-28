053c8d29c66c30b98eba71ef865b874360e88ba9
Nandy <nandychang71@gmail.com>
2025-10-29 00:11:55 +0800

    feat: add GET /course/summary

--- courses/serializers.py
+++ courses/serializers.py
@@ -51,3 +51,16 @@ class CourseListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses
         fields = ("id", "course", "teacher")
+
+
+class CourseSummarySerializer(serializers.Serializer):
+    class BreakdownItem(serializers.Serializer):
+        course = serializers.CharField()
+        userCount = serializers.IntegerField()
+        homeworkCount = serializers.IntegerField()
+        submissionCount = serializers.IntegerField()
+        problemCount = serializers.IntegerField()
+
+    message = serializers.CharField(default="Success.")
+    courseCount = serializers.IntegerField()
+    breakdown = BreakdownItem(many=True)
--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CourseView
+from .views import CourseView, CourseSummaryView
 
 urlpatterns = [
     path('', CourseView.as_view(), name='course'),
+    path('summary/', CourseSummaryView.as_view(), name='course_summary'),
 ]
