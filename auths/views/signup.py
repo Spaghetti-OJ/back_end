972bf21b5cd42f280b6594c9eb7dcec0aa69a0fe
EnLiao <pikachu200594@gmail.com>
2025-11-06 23:45:06 +0800

    refactor: move serial and view from user to auth

---
+++ auths/views/signup.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from auths.serializers import RegisterSerializer, MeSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        return Response(MeSerializer(request.user).data)
