36610d973e8efcc8841e1fecdf05fe0a56cbd449
Nandy <nandychang71@gmail.com>
2025-11-08 23:53:30 +0800

    fix: course_name 改成 course_id

--- courses/urls/course_coursename.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from ..views import course_coursename as detail_views
-
-app_name = "course_coursename"
-
-urlpatterns = [
-    path("<str:course_name>/", detail_views.CourseDetailView.as_view(), name="detail"),
-]
