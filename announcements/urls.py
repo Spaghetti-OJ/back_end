7a7942a2e56ab9d2672579ba79d5fbae0348ed7a
Nandy <nandychang71@gmail.com>
2025-11-09 23:51:13 +0800

    feat: post /ann/

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CourseAnnouncementListView
+from .views import AnnouncementCreateView, CourseAnnouncementListView
 
 app_name = "announcements"
 
 urlpatterns = [
+    path("", AnnouncementCreateView.as_view(), name="create"),
     path("<uuid:course_id>/ann", CourseAnnouncementListView.as_view(), name="course"),
 ]
