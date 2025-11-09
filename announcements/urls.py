7714e6240cab3043a2b4d97cf2208374d28c2d09
Nandy <nandychang71@gmail.com>
2025-11-09 23:34:08 +0800

    feat: get /course/<course_name>/ann

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import CourseAnnouncementListView
+from .views import CourseAnnouncementListView, CourseAnnouncementRetrieveView
 
 app_name = "announcements"
 
 urlpatterns = [
     path("<uuid:course_id>/ann", CourseAnnouncementListView.as_view(), name="course"),
+    path(
+        "<uuid:course_id>/<int:ann_id>",
+        CourseAnnouncementRetrieveView.as_view(),
+        name="announcement",
+    ),
 ]
--- announcements/views.py
+++ announcements/views.py
@@ -6,20 +6,27 @@ from courses.models import Announcements, Courses
 from .serializers import SystemAnnouncementSerializer
 
 
-class CourseAnnouncementListView(generics.GenericAPIView):
+class CourseAnnouncementBaseView(generics.GenericAPIView):
+    serializer_class = SystemAnnouncementSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def _get_course(self, *, course_id):
+        try:
+            return Courses.objects.get(pk=course_id)
+        except Courses.DoesNotExist:
+            return None
+
+
+class CourseAnnouncementListView(CourseAnnouncementBaseView):
     """
     GET /ann/<course_id>/ann - 取得指定課程公告列表。
     若 <course_id> 為「公開討論區」課程的 ID，則等同於原系統公告列表。
     """
 
-    serializer_class = SystemAnnouncementSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
     def get(self, request, *args, **kwargs):
         course_id = kwargs.get("course_id")
-        try:
-            course = Courses.objects.get(pk=course_id)
-        except Courses.DoesNotExist:
+        course = self._get_course(course_id=course_id)
+        if course is None:
             return Response(
                 {"message": "Course not found."}, status=status.HTTP_404_NOT_FOUND
             )
@@ -31,3 +38,32 @@ class CourseAnnouncementListView(generics.GenericAPIView):
         )
         serializer = self.get_serializer(queryset, many=True)
         return Response({"data": serializer.data})
+
+
+class CourseAnnouncementRetrieveView(CourseAnnouncementBaseView):
+    """
+    GET /ann/<course_id>/<ann_id> - 取得單一公告內容。
+    """
+
+    def get(self, request, *args, **kwargs):
+        course_id = kwargs.get("course_id")
+        announcement_id = kwargs.get("ann_id")
+
+        course = self._get_course(course_id=course_id)
+        if course is None:
+            return Response(
+                {"message": "Course not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        try:
+            announcement = Announcements.objects.select_related("creator_id").get(
+                course_id=course, pk=announcement_id
+            )
+        except Announcements.DoesNotExist:
+            return Response(
+                {"message": "Announcement not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = self.get_serializer(announcement)
+        return Response({"data": [serializer.data]})
