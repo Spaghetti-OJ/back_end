bb9002571ce2e90b4ec274a503967656b3033f49
BlankTsai <blank940624@gmail.com>
2025-11-15 16:16:31 +0800

    feat/auth-login-logs-userID

--- auths/urls.py
+++ auths/urls.py
@@ -1,19 +1,13 @@
 # auths/urls.py
-
 from django.urls import path
-
-# 導入你的 views 檔案
-# 你的檔案在 auths/views/login_logs.py
 from .views import login_logs 
 from .views.signup import RegisterView, MeView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views.revoke import SessionRevokeView
 
 urlpatterns = [
-    # 把 'login-logs' 路徑連到 View
-    path('login-logs', login_logs.LoginLogListView.as_view(), name='login-log-list'),
-
-    # ... 這裡未來可以放 'register', 'password-reset' 等路由
+    path('login-logs', login_logs.LoginLogListView.as_view(), name='login-log-list-self'),
+    path('login-logs/<uuid:userId>', login_logs.UserLoginLogListView.as_view(), name='user-login-log-list'),
     path('signup/', RegisterView.as_view(), name='register'),
     path('session/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('session/revoke/', SessionRevokeView.as_view(), name='auth-session-revoke'),
