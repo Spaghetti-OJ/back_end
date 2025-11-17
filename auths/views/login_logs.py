e54b39925ad8a3337428c9334bbc436aec12720d
BlankTsai <blank940624@gmail.com>
2025-11-17 21:59:27 +0800

    fix(auth): apply Copilot suggestions (IsAdminUser, pep8 naming, trailing slash)

--- auths/views/login_logs.py
+++ auths/views/login_logs.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 from ..models import LoginLog
