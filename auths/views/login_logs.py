c5d0809327c4c1c9cd4d260a022ba7521cb1a5bb
EnLiao <pikachu200594@gmail.com>
2025-11-17 21:36:33 +0800

    fix: import isAdmin and add backups to gitignore

--- auths/views/login_logs.py
+++ auths/views/login_logs.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 
 from ..models import LoginLog
