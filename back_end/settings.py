c82b54bf2754c258fee1d784eb61ca466b025429
RokuSennyou <pikachu200594@gmail.com>
2025-12-24 23:54:49 +0800

    feat: update permission class to require email verification for access

--- back_end/settings.py
+++ back_end/settings.py
@@ -70,7 +70,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
         "user.permissions.IsEmailVerified",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
