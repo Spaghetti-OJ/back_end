662451af3fa638b320b052cf09865be6a9a3ba1d
RokuSennyou <pikachu200594@gmail.com>
2025-12-28 16:31:11 +0800

    feat: Update database configuration to use PostgreSQL

--- back_end/settings.py
+++ back_end/settings.py
@@ -122,11 +122,14 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'ATOMIC_REQUESTS': False,
-    }
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.getenv("DB_NAME", "appdb"),
+    "USER": os.getenv("DB_USER", "app"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+    "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+    "PORT": os.getenv("DB_PORT", "5432"),
+  }
 }
 
 # Redis Cache Configuration
